925432c3a844c9f7c7dde3e3806c43aa7c437c8d
Iran Ramirez <33239324+iranramirez@users.noreply.github.com>
2019-04-25 16:48:54 -0500

    Add files via upload

---
+++ test.ino
@@ -0,0 +1,50 @@
+
+int Left_pin = 5;
+int Right_pin = 3;
+char data = 0;
+int data1; 
+int check1, check2; 
+
+void setup( )                    // Initialization settings; then only runs the function.
+
+{
+Serial.begin(9600);             //baudrate is setup to 9600
+pinMode(Left_pin, OUTPUT);
+pinMode(Right_pin, OUTPUT);
+}
+
+void loop( )                    //The function operates repeatedly until the poweroff.
+{
+
+while(Serial.available( ))
+
+{
+
+data = Serial.read();   // Check whether data is available or not. If data is available, then data is read.          
+Serial.write(data);
+if (data == '1')
+{ check1 = HIGH;
+  check2 = LOW;
+}
+else if (data == '2')
+{ check1 = LOW;
+  check2 = HIGH;
+}
+else if (data == '3')
+{ check1 = HIGH;
+  check2 = HIGH;
+}
+else
+{ check1 = LOW;
+  check2 = LOW;
+}
+
+digitalWrite(Left_pin,check1);
+digitalWrite(Right_pin,check2);
+delay(500);
+digitalWrite(Left_pin,LOW);
+digitalWrite(Right_pin,LOW);
+delay(1000);
+
+}
+}
